c4498267468c7bb48adebf9191f73ff927c62aab
thecloudenthusiast <dekoonlay4real@gmail.com>
2024-06-16 20:25:18 -0400

    adding local files into git repo

---
+++ script.js
@@ -0,0 +1,79 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('#game-board');
+    const startButton = document.getElementById('start-game');
+    let cardsChosen = [];
+    let cardsChosenId = [];
+    let cardsWon = [];
+
+    const cardArray = [
+        { name: 'card1', img: 'images/paris.jpeg' },
+        { name: 'card1', img: 'images/paris.jpeg' },
+        { name: 'card2', img: 'images/NewYork.jpeg' },
+        { name: 'card2', img: 'images/NewYork.jpeg' },
+        { name: 'card3', img: 'images/Italy.jpeg' },
+        { name: 'card3', img: 'images/Italy.jpeg' },
+        { name: 'card4', img: 'images/singapore.jpeg' },
+        { name: 'card4', img: 'images/singapore.jpeg' },
+        { name: 'card5', img: 'images/Dubai.jpeg' },
+        { name: 'card5', img: 'images/Dubai.jpeg' },
+        { name: 'card6', img: 'images/London.jpeg' },
+        { name: 'card6', img: 'images/London.jpeg' },
+        // ...add more pairs as needed
+    ];
+
+    function shuffle(array) {
+        array.sort(() => 0.5 - Math.random());
+    }
+
+    function createBoard() {
+        shuffle(cardArray);
+        grid.innerHTML = '';
+        cardsWon = [];
+
+        for (let i = 0; i < cardArray.length; i++) {
+            const card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            card.addEventListener('click', flipCard);
+            grid.appendChild(card);
+        }
+    }
+
+    function flipCard() {
+        let cardId = this.getAttribute('data-id');
+        if (!cardsChosenId.includes(cardId)) {
+            cardsChosen.push(cardArray[cardId].name);
+            cardsChosenId.push(cardId);
+            this.setAttribute('src', cardArray[cardId].img);
+            if (cardsChosen.length === 2) {
+                setTimeout(checkForMatch, 500);
+            }
+        }
+    }
+
+    function checkForMatch() {
+        const cards = document.querySelectorAll('#game-board img');
+        const firstCardId = cardsChosenId[0];
+        const secondCardId = cardsChosenId[1];
+
+        if (cardsChosen[0] === cardsChosen[1] && firstCardId !== secondCardId) {
+            cards[firstCardId].style.visibility = 'hidden';
+            cards[secondCardId].style.visibility = 'hidden';
+            cards[firstCardId].removeEventListener('click', flipCard);
+            cards[secondCardId].removeEventListener('click', flipCard);
+            cardsWon.push(cardsChosen);
+        } else {
+            cards[firstCardId].setAttribute('src', 'images/blank.png');
+            cards[secondCardId].setAttribute('src', 'images/blank.png');
+        }
+
+        cardsChosen = [];
+        cardsChosenId = [];
+
+        if (cardsWon.length === cardArray.length / 2) {
+            alert('Congratulations Darling! You found them all!');
+        }
+    }
+
+    startButton.addEventListener('click', createBoard);
+});
